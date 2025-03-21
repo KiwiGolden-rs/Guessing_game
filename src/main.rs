4c1a94f803f932e51223e59a648f4ab5da139f63
KiwiGolden-rs <QS.object@proton.me>
2025-03-21 15:26:27 +0100

    Update main.rs
    change u32 type by i32

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
 
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("Failed to read line");
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
